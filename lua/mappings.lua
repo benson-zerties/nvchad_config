73870e1b0700d2e881ea8f7e1b1b269f4bc67a07
Benjamin Sackenreuter <ben_sa@gmx.de>
2024-04-22 09:31:54 +0200

    Implement autocmd to open file under cursor from NvChad-Terminal via 'gf'

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,21 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- NvChad terminal module configures an empty "" filetype
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "",
+  callback = function(args)
+    vim.keymap.set('n', 'gf', function()
+      local f = vim.fn.findfile(vim.fn.expand('<cfile>'))
+      print(f)
+      if (f ~= '') then
+        local all_wins = vim.api.nvim_list_wins()
+        local last_win_idx = vim.fn.winnr('#')
+        local last_win_id = all_wins[last_win_idx]
+        vim.api.nvim_set_current_win(last_win_id)
+        vim.cmd.edit(f)
+      end
+    end)
+  end
+})
